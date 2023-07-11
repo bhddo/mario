b8a3b07caff3902dbd303a64fa1ead825afbec55
bhddo <125513079+bhddo@users.noreply.github.com>
2023-07-11 05:32:33 -0300

    Add files via upload

---
+++ mai.js
@@ -0,0 +1,62 @@
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+window.addEventListener("keyDown",  my_KeyDown)
+
+function add() {
+	img_imgTag = new Image(); //definindo uma variável com uma nova imagem
+	img_imgTag.onload = uploadimg; // ajustando uma função, ao carregar essa variável
+	img_imgTag.src = img_image;   // carregar uma imagem
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Escreva um código para obter o evento key-pressed
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//Escreva um código para verificar o tipo de tecla pressionada
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image= "alfabeto (1).png"
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey();
+add()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
